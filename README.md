b577eb39bad85c162b112cec6950162cf33d990a
HaSandhu <sandhunhs@gmail.com>
2024-03-02 01:34:00 -0600

    Added Navbar

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## hjs2245
 
 I got the favicon/logo192/logo512 from here: https://www.flaticon.com/free-icons/helmet
+The homepage logo was generated using DALLE "DALL·E 2024-03-02 01.12.38 - Create three distinct simplistic logos, each showing two Formula 1 cars racing side by side, viewed from a side perspective. The designs should be min"
--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import logo from './logo.png';
 
 function App() {
+  const [activeComponent, setActiveComponent] = useState('raceResults'); // Default component
+
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case 'logo':
+        return <img src={logo} className="App-logo" alt="logo" />;
+      case 'raceResults':
+        return <div>Race Results Component</div>;
+      case 'qualifying':
+        return <div>Qualifying Component</div>;
+      case 'driversChampionship':
+        return <div>Driver's Championship Component</div>;
+      case 'teamsChampionship':
+        return <div>Team's Championship Component</div>;
+      case 'driverInformation':
+        return <div>Driver Information Component</div>;
+      case 'constructorInformation':
+        return <div>Constructor Information Component</div>;
+      case 'circuitInformation':
+        return <div>Circuit Information Component</div>;
+      case 'raceSchedule':
+        return <div>Race Schedule Component</div>;
+      case 'seasonList':
+        return <div>Season List Component</div>;
+      case 'finishingStatus':
+        return <div>Finishing Status Component</div>;
+      case 'lapTimes':
+        return <div>Lap Times Component</div>;
+      case 'pitStops':
+        return <div>Pit Stops Component</div>;
+      case 'about':
+        return <div>About</div>;
+      default:
+        return <div>Race Results Component</div>;
+
+    }
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="navbar navbar-expand navbar-dark">
+          
+          <div className="navbar-nav">
+            <a className="nav-item nav-link" href="#raceResults" onClick={() => setActiveComponent('raceResults')}><img src={logo} className="App-logo" alt="logo" /></a>
+            <a className="nav-item nav-link" href="#raceResults" onClick={() => setActiveComponent('raceResults')}>Race Results</a>
+            <a className="nav-item nav-link" href="#qualifying" onClick={() => setActiveComponent('qualifying')}>Qualifying</a>
+            <a className="nav-item nav-link" href="#driversChampionship" onClick={() => setActiveComponent('')}>Driver's Championship</a>
+            <a className="nav-item nav-link" href="#teamsChampionship" onClick={() => setActiveComponent('teamsChampionship')}>Team's Championship</a>
+            <a className="nav-item nav-link" href="#driverInformation" onClick={() => setActiveComponent('driverInformation')}>Driver Information</a>
+            <a className="nav-item nav-link" href="#constructorInformation" onClick={() => setActiveComponent('constructorInformation')}>Constructor Information</a>
+            <a className="nav-item nav-link" href="#circuitInformation" onClick={() => setActiveComponent('circuitInformation')}>Circuit Information</a>
+            <a className="nav-item nav-link" href="#raceSchedule" onClick={() => setActiveComponent('raceSchedule')}>Race Schedule</a>
+            <a className="nav-item nav-link" href="#seasonList" onClick={() => setActiveComponent('seasonList')}>Season List</a>
+            <a className="nav-item nav-link" href="#finishingStatus" onClick={() => setActiveComponent('finishingStatus')}>Finishing Status</a>
+            <a className="nav-item nav-link" href="#lapTimes" onClick={() => setActiveComponent('lapTimes')}>Lap Times</a>
+            <a className="nav-item nav-link" href="#pitStops" onClick={() => setActiveComponent('pitStops')}>Pit Stops</a>
+            <a className="nav-item nav-link" href="#about" onClick={() => setActiveComponent('about')}>About</a>
+          </div>
+        </nav>
       </header>
+      <div className="main-content">
+        {renderComponent()}
+      </div>
     </div>
   );
 }
-
 export default App;
