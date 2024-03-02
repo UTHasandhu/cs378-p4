1da339435f7efa2542922ed16e4056f428c7369d
HaSandhu <sandhunhs@gmail.com>
2024-03-02 01:46:32 -0600

    ReFactor to move componentList out off App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,20 @@
 import React, { useState } from 'react';
 import './App.css';
+import ComponentList from './componentList'; // Import the ComponentList
 import logo from './logo.png';
 
 function App() {
   const [activeComponent, setActiveComponent] = useState('raceResults'); // Default component
 
-  const renderComponent = () => {
-    switch (activeComponent) {
-      case 'logo':
-        return <img src={logo} className="App-logo" alt="logo" />;
-      case 'raceResults':
-        return <div>Race Results Component</div>;
-      case 'qualifying':
-        return <div>Qualifying Component</div>;
-      case 'driversChampionship':
-        return <div>Driver's Championship Component</div>;
-      case 'teamsChampionship':
-        return <div>Team's Championship Component</div>;
-      case 'driverInformation':
-        return <div>Driver Information Component</div>;
-      case 'constructorInformation':
-        return <div>Constructor Information Component</div>;
-      case 'circuitInformation':
-        return <div>Circuit Information Component</div>;
-      case 'raceSchedule':
-        return <div>Race Schedule Component</div>;
-      case 'seasonList':
-        return <div>Season List Component</div>;
-      case 'finishingStatus':
-        return <div>Finishing Status Component</div>;
-      case 'lapTimes':
-        return <div>Lap Times Component</div>;
-      case 'pitStops':
-        return <div>Pit Stops Component</div>;
-      case 'about':
-        return <div>About</div>;
-      default:
-        return <div>Race Results Component</div>;
-
-    }
-  };
-
-
   return (
     <div className="App">
       <header className="App-header">
         <nav className="navbar navbar-expand navbar-dark">
-          
           <div className="navbar-nav">
-            <a className="nav-item nav-link" href="#raceResults" onClick={() => setActiveComponent('raceResults')}><img src={logo} className="App-logo" alt="logo" /></a>
+            <a className="nav-item nav-link" href="#logo" onClick={() => setActiveComponent('logo')}><img src={logo} className="App-logo" alt="logo" /></a>
             <a className="nav-item nav-link" href="#raceResults" onClick={() => setActiveComponent('raceResults')}>Race Results</a>
             <a className="nav-item nav-link" href="#qualifying" onClick={() => setActiveComponent('qualifying')}>Qualifying</a>
-            <a className="nav-item nav-link" href="#driversChampionship" onClick={() => setActiveComponent('')}>Driver's Championship</a>
+            <a className="nav-item nav-link" href="#driversChampionship" onClick={() => setActiveComponent('driversChampionship')}>Driver's Championship</a>
             <a className="nav-item nav-link" href="#teamsChampionship" onClick={() => setActiveComponent('teamsChampionship')}>Team's Championship</a>
             <a className="nav-item nav-link" href="#driverInformation" onClick={() => setActiveComponent('driverInformation')}>Driver Information</a>
             <a className="nav-item nav-link" href="#constructorInformation" onClick={() => setActiveComponent('constructorInformation')}>Constructor Information</a>
@@ -66,9 +29,10 @@ function App() {
         </nav>
       </header>
       <div className="main-content">
-        {renderComponent()}
+        <ComponentList activeComponent={activeComponent} />
       </div>
     </div>
   );
 }
+
 export default App;
---
+++ src/componentList.js
@@ -0,0 +1,40 @@
+// componentList.js
+import React from 'react';
+import logo from './logo.png';
+
+const ComponentList = ({ activeComponent }) => {
+  switch (activeComponent) {
+    case 'logo':
+      return <img src={logo} className="App-logo" alt="logo" />;
+    case 'raceResults':
+      return <div>Race Results Component</div>;
+    case 'qualifying':
+      return <div>Qualifying Component</div>;
+    case 'driversChampionship':
+      return <div>Driver's Championship Component</div>;
+    case 'teamsChampionship':
+      return <div>Team's Championship Component</div>;
+    case 'driverInformation':
+      return <div>Driver Information Component</div>;
+    case 'constructorInformation':
+      return <div>Constructor Information Component</div>;
+    case 'circuitInformation':
+      return <div>Circuit Information Component</div>;
+    case 'raceSchedule':
+      return <div>Race Schedule Component</div>;
+    case 'seasonList':
+      return <div>Season List Component</div>;
+    case 'finishingStatus':
+      return <div>Finishing Status Component</div>;
+    case 'lapTimes':
+      return <div>Lap Times Component</div>;
+    case 'pitStops':
+      return <div>Pit Stops Component</div>;
+    case 'about':
+      return <div>About</div>;
+    default:
+      return <div>Race Results Component</div>;
+  }
+};
+
+export default ComponentList;
